372b39f2a1b495d238959836a11efdd1360d9fa6
yusufkml <yusf.kaml@gmail.com>
2017-03-14 22:48:29 +0700

    Tugas Output

---
+++ TUGAS-2/Mahasiswa.java
@@ -0,0 +1,9 @@
+public class Mahasiswa {
+	public static void main (String[] args) {
+		System.out.println ("#########################################################");
+		System.out.print ("\n Nama \t: Yusuf Kamaludin");
+		System.out.print ("\n Nim \t: 10113472");
+		System.out.print ("\n Nama \t: PERUM DELUXE RESIDENCE III, Tanah Baru, Depok \n\n");
+		System.out.println ("#########################################################");	
+	}
+}
